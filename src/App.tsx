40741e9e2092a5efe37fd0a50cff2859270df356
RusCherep <ruslik.cherep@gmail.com>
2025-07-01 20:53:05 +0300

    create OnOff.tsx and in this file used hook useState

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 
 import './App.css';
 import {ButtonTemplate1} from "./components/ButtonTemplate";
+import {OnOff} from "./components/OnOff";
 
 function App() {
   return (
     <div >
-      <ButtonTemplate1 isOn={true}/>
-      <ButtonTemplate1 isOn={false}/>
+
+        <OnOff />
+
+      {/*<ButtonTemplate1 isOn={true}/>
+      <ButtonTemplate1 isOn={false}/>*/}
     </div>
   );
 }
